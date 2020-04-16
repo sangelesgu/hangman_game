95a6531e926f3264cafd865bbe0a16d7e9ed8c74
sangelesgu <s.angelesgu@gmail.com>
2020-04-16 20:29:31 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ahorcado';
+  title = 'app';
+
+  palabra = "AGUACATE";
+  palabraOculta = "";
+
+  intentos = 0;
+
+  gano = false;
+  perdido = false;
+
+
+  letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+  'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
+  'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  constructor(){
+    this.palabraOculta = '_ '.repeat(this.palabra.length)
+
+  }
+
+  comprobar( letra ){
+
+    this.existeLetra(letra);
+    let palabraOcultaArr = this.palabraOculta.split(' ');
+
+    for (let i = 0; i < this.palabra.length; i++){
+      if (this.palabra[i] === letra) {
+        palabraOcultaArr[i] = letra;
+      }
+    }
+
+    this.palabraOculta = palabraOcultaArr.join(' ');
+    this.verificaResultado();
+  }
+
+  verificaResultado(){
+    const palabraArr = this.palabraOculta.split(' ');
+    const palabraEvaluar = palabraArr.join('');
+
+    if (palabraEvaluar === this.palabra) {
+      this.gano = true;
+
+    }
+
+    if (this.intentos >= 9 ) {
+      this.perdido = true;
+
+    }
+
+
+  }
+
+  existeLetra (letra){
+    if (this.palabra.indexOf(letra) >= 0) {
+
+    }else{
+
+      this.intentos++
+    }
+  }
+
 }
